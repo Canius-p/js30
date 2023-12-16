c215bb0580e5ff84af3e5d48300bd9eb0fd5b5e0
Canius-p <boiinventer@gmail.com>
2023-12-16 08:56:45 +0545

    hash table

---
+++ hashTable.js
@@ -0,0 +1,52 @@
+//create hash table
+var hash = (string, max) => {
+  var hash = 0;
+  for (var i = 0; i < string.length; i++) {
+    hash += string.charCodeAt(i);
+  }
+  return hash % max;
+};
+let HashTable = function () {
+  let storage = [];
+  const storageLimit = 4;
+  this.print = function () {
+    console.log(storage);
+  };
+  this.add = function (key, value) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      var inserted = false;
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.lookup = function (key) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+};
+console.log(hash("nareshs", 20));
+let ht = new HashTable();
+ht.add("ram", "person");
+ht.add("naresh", "person");
+ht.add("bob", "dog");
+console.log(ht.lookup("bob"));
+ht.print();
